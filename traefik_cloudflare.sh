1e00f0a3fdc9e9ff6a3fcee2ca7a541336079e75
SolutionHubBosnia <solutionhubbosnia@gmail.com>
2025-04-24 13:05:56 +0200

    Update traefik_cloudflare.sh

--- traefik_cloudflare.sh
+++ traefik_cloudflare.sh
@@ -1,42 +1,47 @@
 #!/bin/bash
 set -e
 
+# Check if script is run as root, otherwise restart with sudo
+# Provjerava da li je skripta pokrenuta kao root, u suprotnom se restartuje sa sudo
 if [[ "$EUID" -ne 0 ]]; then
-  echo "Greška: Skripta zahtijeva root prava. Ponovno pokretanje sa sudo..."
-  echo "Error: Script requires root. Restart with sudo..."
+  echo "Error: Script requires root privileges. Restarting with sudo..."
+  echo "Greška: Skripta zahtijeva root privilegije. Ponovno pokretanje sa sudo..."
   exec sudo "$0" "$@"
 fi
 
+# Function to print instructions for creating Cloudflare API Token
+# Funkcija za ispis uputa kako kreirati Cloudflare API token
 print_instructions() {
-  printf "Koraci za generisanje CLOUDFLARE_API_TOKEN-a:\n"
-  printf "1. Prijavite se na svoj Cloudflare račun.\n"
-  printf "2. Idite na Profil.\n"
-  printf "3. Kliknite na API tokeni sa lijeve strane.\n"
-  printf "4. Kliknite na Kreiraj token.\n"
-  printf "5. Odaberite predložak "Edit zone DNS".\n"
-  printf "6. Dodijelite naziv tokenu i prava:\n"
-  printf "   Permissions: Zone/DNS/Edit"
-  printf "   Zone Resources: Include/Specific zone/select domain name"
-  printf "7. Kliknite na Stvori token.\n"
-  printf "8. Kopirajte tajnu tokena i sačuvajte je na sigurnom mjestu a zatim podesite na Cloaudflare prodilu domene Current encryption mode: Full (strict).\n"
-  printf "#############################################"
-  printf "Steps to generate a CLOUDFLARE_API_TOKEN:\n"
+  printf "Steps to generate CLOUDFLARE_API_TOKEN:\n"
   printf "1. Log in to your Cloudflare account.\n"
   printf "2. Go to Profile.\n"
   printf "3. Click on API Tokens on the left.\n"
-  printf "4. Click on Create Token.\n"
-  printf "5. Select the "Edit zone DNS" template.\n"
-  printf "6. Assign a name and permissions to the token:\n"
-  printf " Permissions: Zone/DNS/Edit"
-  printf " Zone Resources: Include/Specific zone/select domain name"
-  printf "7. Click on Create Token.\n"
-  printf "8. Copy the token secret and save it in a safe place and then set it to Cloudflare domain extension Current encryption mode: Full (strict).\n"
+  printf "4. Click Create Token.\n"
+  printf "5. Choose a template or create a custom token.\n"
+  printf "6. Assign permissions:\n"
+  printf "   - Zone: Read\n"
+  printf "   - DNS: Edit\n"
+  printf "   - SSL/TLS: Read\n"
+  printf "7. Click Create Token and save it in a secure location.\n"
+  printf "_______________________________________________________"
+  printf "Koraci za generiranje CLOUDFLARE_API_TOKEN:\n"
+  printf "1. Prijavite se na svoj Cloudflare račun.\n"
+  printf "2. Idi na profil.\n"
+  printf "3. Kliknite na API tokene na lijevoj strani.\n"
+  printf "4. Kliknite Kreiraj token.\n"
+  printf "5. Odaberite predložak ili kreirajte prilagođeni token.\n"
+  printf "6. Dodijeli dozvole:\n"
+  printf " - Zona: Čitanje\n"
+  printf " - DNS: Uredi\n"
+  printf " - SSL/TLS: Čitaj\n"
+  printf "7. Kliknite Kreiraj token i spremite ga na sigurnu lokaciju.\n"
+  
 }
 
 print_instructions
 
-# Funkcija za siguran unos povjerljivih podataka sa prikazom zvjezdica
-# Function for secure entry of confidential data with star display
+# Function for secure input of sensitive data
+# Funkcija za siguran unos osjetljivih podataka
 secure_read() {
   local prompt="$1"
   local var_name="$2"
@@ -61,73 +66,81 @@ secure_read() {
   eval "$var_name='$input'"
 }
 
-secure_read "Unesite vaš Cloudflare API token (Enter your Cloudflare API token): " CLOUDFLARE_API_TOKEN
-read -p "Unesite vašu Cloudflare email adresu (Enter your Cloudflare email address): " CLOUDFLARE_EMAIL
-read -p "Unesite naziv poddomene za Traefik (Enter a subdomain name for Traefik): " DOMAIN_NAME
-read -p "Unesite naziv emaila za Let's Encrypt (Enter your Let's Encrypt email name): " LETSENCRYPT_EMAIL
-secure_read "Unesite korisničko ime za osnovnu autentifikaciju (Enter your username for basic authentication.): " AUTH_USER
-secure_read "Unesite lozinku za osnovnu autentifikaciju (Enter a password for basic authentication): " AUTH_PASSWORD
+# User input
+# Unos korisničkih podataka
+secure_read "Enter your Cloudflare API token/Unesite svoj Cloudflare API token: " CLOUDFLARE_API_TOKEN
+read -p "Enter your Cloudflare email address/Unesite svoju Cloudflare adresu e-pošte: " CLOUDFLARE_EMAIL
+read -p "Enter your subdomain name for Traefik/Unesite ime vaše poddomene za Traefik: " DOMAIN_NAME
+secure_read "Enter username for Traefik Dashboard basic authentication/Unesite korisničko ime za osnovnu autentifikaciju Traefik Dashboard: " AUTH_USER
+secure_read "Enter password for Traefik Dashboard basic authentication/Unesite lozinku za osnovnu autentifikaciju Traefik Dashboard: " AUTH_PASSWORD
 
-for var in CLOUDFLARE_API_TOKEN CLOUDFLARE_EMAIL DOMAIN_NAME LETSENCRYPT_EMAIL AUTH_USER AUTH_PASSWORD; do
+# Check if all variables are provided
+# Provjerava da li su svi podaci uneseni
+for var in CLOUDFLARE_API_TOKEN CLOUDFLARE_EMAIL DOMAIN_NAME AUTH_USER AUTH_PASSWORD; do
   if [[ -z "${!var}" ]]; then
-    echo "Greška: Svi unosi su obavezni."
-    echo "Error: All entries are required."
+    echo "Error: All inputs are required."
+    echo "Greška: svi ulazni podatci su potrebni."
     exit 1
   fi
 done
 
-mkdir -p traefik
+# Create necessary directories and files
+# Kreira potrebne direktorije i fajlove
+mkdir -p traefik/data/configurations traefik/letsencrypt
+touch traefik/letsencrypt/acme.json
+chmod 600 traefik/letsencrypt/acme.json
 
+echo "acme.json file created with correct permissions."
+echo "acme.json fajl kreiran sa ispravnim dozvolama."
+
+# Install htpasswd tool if not present
+# Instalira htpasswd alat ako nije prisutan
 if ! command -v htpasswd &> /dev/null; then
-  echo "htpasswd alat nije pronađen. Instalacija..."
   echo "htpasswd tool not found. Installing..."
+  echo "htpasswd alat nije pronađen. Instaliranje..."
   if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sudo apt-get update
     sudo apt-get install apache2-utils -y
   elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install httpd
   else
-    echo "Greška: Nije podržana platforma za automatsku instalaciju htpasswd alata."
-    echo "Error: Platform not supported for automatic installation of htpasswd tool."
+    echo "Error: Unsupported platform for automatic htpasswd installation."
+    echo "Greška: Nepodržana platforma za automatsku instalaciju htpasswd."
     exit 1
   fi
 fi
 
+# Generate hashed password and write to file
+# Generiše hashiranu lozinku i zapisuje u fajl
 HASHED_USER=$(htpasswd -nb "$AUTH_USER" "$AUTH_PASSWORD")
 echo "$HASHED_USER" > traefik/.traefikpasswd
 
 if [[ ! -f "traefik/.traefikpasswd" ]]; then
-  echo "Greška: .traefikpasswd fajl nije kreiran."
-  echo "Error: .traefikpasswd file not created."
+  echo "Error: .traefikpasswd file was not created."
+  echo "Greška: .traefikpasswd datoteka nije kreirana."
   exit 1
 else
-  echo "Fajl .traefikpasswd kreiran uspješno."
-  echo "File .traefikpasswd created successfully."
+  echo ".traefikpasswd file created successfully."
+  echo ".traefikpasswd datoteka uspješno kreirana."
 fi
 
+# Write environment variables to .env file
+# Zapisuje varijable okruženja u .env fajl
 cat <<EOF > traefik/.env
 CF_API_TOKEN="$CLOUDFLARE_API_TOKEN"
 CF_API_EMAIL="$CLOUDFLARE_EMAIL"
 DOMAIN_NAME="$DOMAIN_NAME"
-LETSENCRYPT_EMAIL="$LETSENCRYPT_EMAIL"
-HOST_NAME="$DOMAIN_NAME"
 EOF
 
 if [[ ! -f "traefik/.env" ]]; then
-  echo "Greška: .env fajl nije kreiran."
-  echo "Error: .env file not created."
+  echo "Error: .env file was not created."
   exit 1
 else
-  echo ".env fajl kreiran uspješno."
   echo ".env file created successfully."
 fi
 
-mkdir -p traefik/data/configurations traefik/certificates
-echo "Direktoriji kreirani uspješno."
-echo "Directories created successfully."
-
-# Generisanje docker-compose.yml fajla koristeći heredoc
-# Generating a docker-compose.yml file using heredoc
+# Generate docker-compose.yml for Traefik
+# Generiše docker-compose.yml za Traefik
 cat <<EOF > traefik/docker-compose.yml
 networks:
   traefik:
@@ -137,66 +150,82 @@ networks:
 services:
   traefik:
     container_name: traefik
-    image: traefik:latest
+    image: traefik:v3.3.3
+    restart: unless-stopped
+
     command:
-      - --api=true
       - --api.dashboard=true
-      - --certificatesresolvers.letsencrypt.acme.dnschallenge.delaybeforecheck=0
-      - --certificatesresolvers.letsencrypt.acme.dnschallenge.provider=cloudflare
-      - --certificatesresolvers.letsencrypt.acme.dnschallenge.resolvers=1.1.1.1:53,8.8.8.8:53
-      - --certificatesresolvers.letsencrypt.acme.dnschallenge=true
-      - --certificatesresolvers.letsencrypt.acme.email=\${CF_API_EMAIL}
-      - --certificatesresolvers.letsencrypt.acme.storage=/letsencrypt/acme.json
+      - --log.level=INFO
       - --entrypoints.http.address=:80
       - --entrypoints.https.address=:443
-      - --entryPoints.https.http3
-      - --entryPoints.https.http3.advertisedport=443
+      - --entrypoints.https.http.tls=true
       - --providers.docker=true
       - --providers.docker.exposedbydefault=false
       - --providers.file.directory=/etc/traefik/dynamic
+      - --certificatesresolvers.cloudflare.acme.storage=/letsencrypt/acme.json
+      - --certificatesresolvers.cloudflare.acme.dnschallenge.provider=cloudflare
+      - --certificatesresolvers.cloudflare.acme.dnschallenge.delaybeforecheck=0
+
     env_file:
       - ./.env
+
     environment:
       - CLOUDFLARE_DNS_API_TOKEN=\${CF_API_TOKEN}
-    hostname: \${HOST_NAME}
-    labels:
-      - traefik.enable=true
-      - traefik.http.middlewares.auth.basicauth.usersfile=/etc/traefik/.traefikpasswd
-      - traefik.http.middlewares.to-https.redirectscheme.scheme=https
-      - traefik.http.routers.to-https.entrypoints=http
-      - traefik.http.routers.to-https.middlewares=to-https
-      - traefik.http.routers.to-https.rule=HostRegexp(\`{host:.+}\`)
-      - traefik.http.routers.traefik.entrypoints=https
-      - traefik.http.routers.traefik.middlewares=auth
-      - traefik.http.routers.traefik.rule=Host(\`\${HOST_NAME}\`)
-      - traefik.http.routers.traefik.service=api@internal
-      - traefik.http.routers.traefik.tls.certresolver=letsencrypt
-      - traefik.http.routers.traefik.tls=true
-      - traefik.http.routers.dashboard.entrypoints=https
-      - traefik.http.routers.dashboard.rule=Host(\`\${DOMAIN_NAME}\`)
-      - traefik.http.routers.dashboard.service=api@internal
-      - traefik.http.routers.dashboard.tls.certresolver=letsencrypt
-      - traefik.http.routers.dashboard.tls=true
-    networks:
-      - traefik
+    hostname: \${DOMAIN_NAME}
+
     ports:
       - 80:80
       - 443:443/tcp
       - 443:443/udp
-      - 8080:8080
-    restart: unless-stopped
+
+    networks:
+      - traefik
+
     volumes:
       - /var/run/docker.sock:/var/run/docker.sock:ro
-      - ./acme:/letsencrypt
+      - ./letsencrypt:/letsencrypt
       - ./data:/etc/traefik/dynamic
       - ./.traefikpasswd:/etc/traefik/.traefikpasswd:ro
+
+    labels:
+      - traefik.enable=true
+
+      # Basic Auth Middleware
+      # Middleware za osnovnu autentifikaciju
+      - traefik.http.middlewares.auth.basicauth.usersfile=/etc/traefik/.traefikpasswd
+      - traefik.http.middlewares.auth.basicauth.removeheader=true
+
+      # Redirect HTTP to HTTPS
+      # Redirekcija HTTP ka HTTPS
+      - traefik.http.middlewares.to-https.redirectscheme.scheme=https
+      - traefik.http.routers.to-https.entrypoints=http
+      - traefik.http.routers.to-https.middlewares=to-https
+      - traefik.http.routers.to-https.rule=HostRegexp(\`{host:.+}\`)
+
+      # Traefik Dashboard
+      # Traefik kontrolna tabla
+      - traefik.http.routers.traefik.entrypoints=https
+      - traefik.http.routers.traefik.middlewares=auth
+      - traefik.http.routers.traefik.rule=Host(\`\${DOMAIN_NAME}\`)
+      - traefik.http.routers.traefik.service=api@internal
+      - traefik.http.routers.traefik.tls.certresolver=cloudflare
+      - traefik.http.routers.traefik.tls=true
+
+      # Dashboard Router
+      # Ruter za dashboard
+      - traefik.http.routers.dashboard.entrypoints=https
+      - traefik.http.routers.dashboard.rule=Host(\`\${DOMAIN_NAME}\`)
+      - traefik.http.routers.dashboard.service=api@internal
+      - traefik.http.routers.dashboard.tls.certresolver=cloudflare
+      - traefik.http.routers.dashboard.tls=true
+      - traefik.http.routers.dashboard.middlewares=auth
 EOF
 
-echo "docker-compose.yml fajl kreiran uspješno."
-echo "docker-compose.yml file created successfully."
+echo "docker-compose.yml for Traefik created successfully."
+echo "docker-compose.yml za Traefik uspješno kreiran."
 
-# Generisanje tls.yml fajla
-# Generating the tls.yml file
+# Generate tls.yml file with TLS security settings
+# Generiše tls.yml fajl sa TLS sigurnosnim postavkama
 cat <<EOF > traefik/data/configurations/tls.yml
 tls:
   options:
@@ -214,29 +243,33 @@ tls:
         - h3
 EOF
 
-echo "tls.yml fajl kreiran uspješno."
 echo "tls.yml file created successfully."
+echo "tls.yml fajl uspješno kreiran."
 
+# Create docker network for Traefik if it doesn't exist
+# Kreira docker mrežu za Traefik ako ne postoji
 if ! docker network inspect traefik >/dev/null 2>&1; then
-  echo "Kreiranje Traefik mreže..."
   echo "Creating Traefik network..."
+  echo "Kreiranje Traefik mreže..."
   docker network create traefik
-  echo "Traefik mreža kreirana uspješno."
-  echo "Traffic network created successfully."
+  echo "Traefik network created successfully."
+  echo "Traefik mreža uspješno kreirana."
 else
+  echo "Traefik network already exists."
   echo "Traefik mreža već postoji."
-  echo "Traffic network already exists."
 fi
 
-read -p "Želite li automatski pokrenuti Traefik? (da/ne) / Do you want to start Traefik automatically? (yes=da/no=ne): " AUTOMATIC_START
-if [ "$AUTOMATIC_START" == "da" ]; then
-  echo "Pokrećem Traefik..."
+# Ask user if Traefik should be started automatically
+# Pita korisnika da li želi automatski pokrenuti Traefik
+read -p "Do you want to automatically start Traefik? (Y/N)/Želite li automatski pokrenuti Traefik? (Y/N): " AUTOMATIC_START
+if [ "$AUTOMATIC_START" == "Y" ]; then
   echo "Starting Traefik..."
+  echo "Početak Traefik..."
   cd traefik
   docker-compose up -d || docker compose up -d
+  echo "Traefik started successfully."
   echo "Traefik je uspješno pokrenut."
-  echo "Traffic started successfully."
 else
-  echo "Traefik nije automatski pokrenut. Možete ga ručno pokrenuti sa 'docker-compose up -d' ili 'docker compose up -d' u direktoriju 'traefik'."
-  echo "Traefik is not started automatically. You can start it manually with 'docker-compose up -d' or 'docker compose up -d' in the 'traefik' directory."
+  echo "Traefik was not started. You can start it manually with 'docker-compose up -d' or 'docker compose up -d' from the 'traefik' directory."
+  echo "Traefik nije pokrenut. Možete ga pokrenuti ručno sa 'docker-compose up -d' ili 'docker compose up -d' iz 'traefik' direktorija."
 fi
